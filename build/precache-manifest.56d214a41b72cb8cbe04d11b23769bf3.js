0fccfef05d2ae5f2f920c25af3dd263cd5be71bc
rahul <rahul999@gmail.com>
2019-03-18 02:10:37 -0400

    moving files around for travis

---
+++ build/precache-manifest.56d214a41b72cb8cbe04d11b23769bf3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "787be91474ccca570bdc",
+    "url": "/static/css/main.8d49c180.chunk.css"
+  },
+  {
+    "revision": "787be91474ccca570bdc",
+    "url": "/static/js/main.787be914.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "67ea9b7a3d8dbd98365f71ae858557a0",
+    "url": "/index.html"
+  }
+];
